32a6d0665d4af7b2cde63c7cd5da5b51dcedb4eb
CORPO\AZajaczkowski <artur.zajaczkowski@t-mobile.pl>
2021-03-18 20:42:08 +0100

    Add new app base on datetime module

--- time_till_deadline.py
+++ time_till_deadline.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 
 user_input = input("Enter your goal with your deadline separated with colon\n")
 input_list = user_input.split(":")
@@ -6,8 +6,8 @@ input_list = user_input.split(":")
 goal = input_list[0]
 deadline = input_list[1]
 
-deadline_date = datetime.datetime.strptime(deadline, "%d.%m.%Y")
-today = datetime.datetime.today()
+deadline_date = datetime.strptime(deadline, "%d.%m.%Y")
+today = datetime.today()
 #calculation how many days from now till deadline
 
 time_till_days_to_deadline = deadline_date - today
